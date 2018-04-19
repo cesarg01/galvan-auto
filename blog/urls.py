9bac517fad9a65b4bd2ea6134b30d302da14e6e2
Cesar Gonzalez <gbrothers93@gmail.com>
2018-04-19 09:59:45 -0700

    Added a better site design to the index page

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,5 @@ from . import views
 
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+    url(r'^$', views.index_page, name='index'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def post_list(request):
+def index_page(request):
     # The function render will render (put together) our template blog/post_list.html
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/index.html', {})
